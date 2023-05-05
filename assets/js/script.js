61663bdd882191626fc780893c6ddf237026a799
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-05-06 00:48:58 +0100

    Removed teacher comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
 // The .ready() function will execute this code after the DOM is ready
   $(document).ready(function() {
+
 
       // TODO: Add code to display the current date in the header of the page.
     var now = dayjs()
     $('#currentDay').text(now.format('dddd, MMMM d'));
 
-      // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
 
   function checkHour() {
   
@@ -36,14 +28,9 @@
   });
   }
 
+
 checkHour();
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
 
 // Registers a click event on the .saveBtn class buttons
 $('.saveBtn').on('click', function () {
@@ -57,10 +44,6 @@ $('.saveBtn').on('click', function () {
 localStorage.setItem(theID, msg);
 });
 
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
   // Retrieves information from the localStorage
   // Inserts localStorage items into the value of the various descriptions
